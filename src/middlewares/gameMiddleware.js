33bc229405872f9eba088a663e3cff547e8becf2
felipe <felipe.dreis.monteiro@gmail.com>
2022-12-08 16:05:16 -0300

    feat: GET POST  games route

---
+++ src/middlewares/gameMiddleware.js
@@ -0,0 +1,23 @@
+import { connection } from "../database/db.js";
+import { gameSchema } from "../models/gameSchema.js";
+
+export default async function gameMiddleware(req, res, next) {
+    try {
+        const { name, categoryId } = req.body;
+        const validate = await gameSchema.validateAsync(req.body, {abortEarly: false});
+        const categorie = await connection.query("SELECT * FROM categories WHERE id=$1", [categoryId])
+        const nameFounded = await connection.query("SELECT * FROM games WHERE name=$1", [name]);
+
+        if (categorie.rows.length === 0) {
+            return res.sendStatus(400);
+        } else if(nameFounded.rows.length !== 0) {
+            return res.sendStatus(409);
+        }
+
+        req.validate = validate;
+
+        next();
+    } catch(err) {
+        res.send(err.details.map(d => d.message));
+    }
+}
