47851bedd2a22a995d8890b7d4e89dca348c0e48
felipe <felipe.dreis.monteiro@gmail.com>
2022-12-09 15:40:08 -0300

    fix: get customers

--- src/controllers/customersGetController.js
+++ src/controllers/customersGetController.js
@@ -2,10 +2,15 @@ import { connectionDB } from "../database/db.js";
 
 export default async function customersController(req, res) {
     try {
-        const cpf = req.query;
-        const customers = await connectionDB.query(`SELECT * FROM customers WHERE cpf=$1`, [cpf]);
+        const cpf = req.query.cpf;
+        if (cpf) {
+            const customers = await connectionDB.query(`SELECT * FROM customers WHERE cpf LIKE ( $1||'%')`, [cpf]);
+            return res.send(customers.rows);
+        };  
+        const customers = await connectionDB.query(`SELECT * FROM customers`);
         res.send(customers.rows);
     } catch(err) {
+        console.log(err);
         res.send(err.message);
     }
 }
